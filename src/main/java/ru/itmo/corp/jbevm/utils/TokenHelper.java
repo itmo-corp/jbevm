ef63f9d0403063a131fb43ec86f8bbed812e0c80
maxbarsukov <maximbarsukov@bk.ru>
2022-09-25 22:50:50 +0300

    feat: :sparkles: use TokenHelper for input stream symbols classification

---
+++ src/main/java/ru/itmo/corp/jbevm/utils/TokenHelper.java
@@ -0,0 +1,17 @@
+package ru.itmo.corp.jbevm.utils;
+
+public class TokenHelper {
+    public static boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    public static boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    public static boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+}
